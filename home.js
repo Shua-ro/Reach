99699378caa181adf9f1351f890d3c5a4d5cc423
Joshua <joshua.ramirez.academic@gmail.com>
2026-01-12 09:50:04 +0800

    login page 95% done

--- home.js
+++ home.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
+
 const mobileMenu = document.querySelector('.tabletnav-container');
 const storedName = localStorage.getItem('userName');
-const nameElement = document.querySelector('.username');
+const nameElement = document.querySelectorAll('.username');
+const noBtnElement = document.querySelector('.no-btn');
 
 hamburger.addEventListener('click', () => {
     
@@ -11,5 +13,20 @@ hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
 });
 if(storedName){
-    nameElement.innerText = storedName;
+    nameElement[0].innerText = storedName;
+    nameElement[1].innerText = storedName;
 }
+const profileMenu = document.querySelector('.user-icon');
+const logoutBtn = document.querySelector('.goback');
+const profileMenuH = document.querySelector('.user-iconH');
+
+profileMenu.addEventListener('click', ()=>{
+    logoutBtn.classList.toggle('active');
+});
+profileMenuH.addEventListener('click', ()=>{
+    logoutBtn.classList.toggle('active');
+});
+noBtnElement.addEventListener('click', ()=>{
+    logoutBtn.classList.toggle('active');
+});
+
--- login.js
+++ login.js
@@ -13,6 +13,9 @@ const confirmPassword = document.getElementById('confirm-password');
 const errorElement = document.querySelector('.error');
 const nameS = document.getElementById('nick-name')
 
+const passErrorElement = document.querySelector('.passerror');
+
+/* This is Where the Sign In and Sign up Button js to change colors and active buttons */
 signUpTab.addEventListener('click', () => {
 
     signUpTab.classList.remove("active-btn");
@@ -56,7 +59,7 @@ signInTab.addEventListener('click', () => {
 });
 
 
-
+/* This is for when The Login or Sign Up button is clicked */
 mainBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
@@ -70,7 +73,7 @@ mainBtn.addEventListener('click', (e) => {
     const passVal = passwordEl.value;
     const confirmVal = confirmEl.value;
 
-
+    /* If email and password is empty or null we display alert */
     if (!emailVal) {
         alert("Please enter your email!");
         return;
@@ -81,14 +84,14 @@ mainBtn.addEventListener('click', (e) => {
         return;
     }
 
-
+    /* This says "If we are in the Sign In section" */
     if (signInTab.classList.contains('active-btn')) {
 
 
         const storedEmail = localStorage.getItem('userEmail');
         const storedPass = localStorage.getItem('userPassword');
 
-
+        /* If the Email/password matches the local storage or if there is an email/password in the local storage*/
         if (!storedEmail) {
             alert("No account found! Please Sign Up first.");
             return;
@@ -107,21 +110,21 @@ mainBtn.addEventListener('click', (e) => {
             return;
         }
 
-
+        /* we print to check if working */
         console.log("Login Successful");
         window.location.href = 'home.html';
 
 
     }
-
+    /* Else if we are not in Sign In tab and instead, we are in the Sign Up tab */
     else {
 
-
+        /* If there is no name input and name is too long */
         if (!nameVal) {
             alert("Please enter your name!");
             return;
         }
-        if (nameVal.length > 10) {
+        if (nameVal.length > 14) {
 
             errorElement.classList.remove('hidden');
 
@@ -129,8 +132,16 @@ mainBtn.addEventListener('click', (e) => {
 
             return;
         }
+        /* If password is too short */
 
+        if(passVal.length < 6){
+            passErrorElement.classList.remove('hidden');
+            
+            console.log(passErrorElement);
+            return;
+        }
 
+        /* If the password does not equal confirm password */
         if (passVal !== confirmVal) {
             alert("Passwords do not match!");
             confirmEl.value = "";
@@ -182,15 +193,15 @@ if (appleBtn) {
     });
 }
 
-if(nameS) {
+if (nameS) {
     nameS.addEventListener('focus', () => {
         errorElement.classList.add('hidden');
     });
-    
+
 }
-if(nameS) {
+if (nameS) {
     nameS.addEventListener('input', () => {
         errorElement.classList.add('hidden');
     });
-    
+
 }
