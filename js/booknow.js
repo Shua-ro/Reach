0bfdd0fc8c1d6ffbaa3371c497115fc7c6c28d46
Joshua <joshua.ramirez.academic@gmail.com>
2026-01-20 23:28:26 +0800

    finals output, code is done

--- js/booknow.js
+++ js/booknow.js
@@ -77,3 +77,65 @@ resultsNumber[3].addEventListener('click', (e) => {
     resultsNumber[3].classList.add('active');
     showing.innerText = '03';
 });
+resultsNumber[4].addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('clicked');
+    results[0].classList.add('hidden');
+    results[1].classList.add('hidden');
+    results[2].classList.add('hidden');
+    results[3].classList.remove('hidden');
+
+    resultsNumber[1].classList.remove('active');
+    resultsNumber[2].classList.remove('active');
+    resultsNumber[3].classList.remove('active');
+    resultsNumber[4].classList.add('active');
+    showing.innerText = '04';
+});
+
+const popupDetails = document.getElementById('popup-details');
+const closeBtn = document.querySelector('.close');
+const detailsButtons = document.querySelectorAll('.details-link');
+
+
+const titleDetails = document.getElementById('title-details');
+const titleLocation = document.getElementById('title-location');
+const detailPrice = document.getElementById('detail-price');
+const detailImage = document.getElementById('detImage'); 
+
+
+detailsButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const card = btn.closest('.result-card');
+
+        
+        const title = card.querySelector('h4').innerText;
+        const location = card.querySelector('.loc').innerText;
+        const price = card.querySelector('.price-line span').innerText;
+
+        const style = window.getComputedStyle(card.querySelector('.picture'));
+        const bgImage = style.backgroundImage.slice(5, -2);
+
+        titleDetails.innerText = title;
+        titleLocation.innerText = location;
+        detailPrice.innerText = price;
+        detailImage.src = bgImage;
+
+        popupDetails.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+const closePopup = () => {
+    popupDetails.classList.remove('active');
+    document.body.style.overflow = 'auto';
+};
+
+closeBtn.addEventListener('click', closePopup);
+
+window.addEventListener('click', (e) => {
+    if (e.target === popupDetails) {
+        closePopup();
+    }
+});
